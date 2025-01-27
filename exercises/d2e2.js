8e2c981578a6f57ed0eb58040cd91f698a917678
Charlie-robin <charlie.robin.richardson@gmail.com>
2025-01-27 14:16:36 +0000

    Day 2 Exercises added

--- exercises/d2e2.js
+++ exercises/d2e2.js
@@ -1,13 +1,17 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
-app.set('view engine', 'ejs')
-app.set('views', 'exercises/views')
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "exercises/views");
 
 // TODO: Configure the app to serve static files from 'exercises/public'
+app.use(express.static("exercises/public"));
 
 // TODO: Add a get handler for '/d2e2' which renders 'd2e2.ejs'
-
+app.get("/d2e2", (req, res) => {
+  // TODO: Render 'd2e1.ejs'
+  res.render("d2e2.ejs");
+});
 // TODO: Open d2e2.ejs and follow the instructions
 
-export default app
+export default app;
