88ae0baa7a7a05d6f47dcf7b69d9f430d0c6541d
Charlie-robin <charlie.robin.richardson@gmail.com>
2025-01-27 14:04:18 +0000

    Day 1 Exercises added

--- exercises/d1e3.js
+++ exercises/d1e3.js
@@ -5,18 +5,21 @@ app.set('views', 'exercises/views') // <-- Don't change
 
 // TODO: Configure the app to use EJS as the view engine
 
-app.get('/d1e3', (req, res) => {
+app.set("view engine", "ejs");
+
+app.get("/d1e3", (req, res) => {
   const shopping = [
-    'Eggs',
-    'Flour',
-    'Sugar',
-    'Lifesize cutout of Christian Bale as Batman',
-    'Milk',
-    'Bread'
-  ]
+    "Eggs",
+    "Flour",
+    "Sugar",
+    "Lifesize cutout of Christian Bale as Batman",
+    "Milk",
+    "Bread",
+  ];
 
   // TODO: Render 'd1e3.ejs', passing the value of `shopping
-})
+  res.render("d1e3.ejs", { shopping });
+});
 
 // TODO: Open d1e3.ejs and follow the instructions
 
